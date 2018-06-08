5cae435293404d822f429cb5aba1a4c2748ba599
XiaoXiao <279599829@qq.com>
2018-06-08 14:10:53 +0800

    ...

--- src/applications/search/controller/PhabricatorApplicationSearchController.php
+++ src/applications/search/controller/PhabricatorApplicationSearchController.php
@@ -364,8 +364,20 @@ final class PhabricatorApplicationSearchController
                   }
               }
 
+              $objects_temp = array();
               if (count($objects_resolved_temp)!=0 && count($objects_released_temp)!=0){
-                  $objects_temp = array_intersect_assoc($objects_resolved_temp, $objects_released_temp);
+                  $objects_phid = array();
+                  foreach($objects_resolved_temp as $k=>$v){
+                      $objects_phid[] = $v->getPHID();
+                  }
+
+                  foreach($objects_released_temp as $k=>$v){
+                      foreach ($objects_phid as $object_phid){
+                          if(strcmp($v->getPHID(),$object_phid) == 0){
+                              $objects_temp[$k]=$v;
+                          }
+                      }
+                  }
               }else{
                   $objects_temp = array_merge($objects_resolved_temp,$objects_released_temp);
               }
