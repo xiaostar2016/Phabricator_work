ab49c3d995b09a5b669d5e058f8c56260ee575eb
XiaoXiao <279599829@qq.com>
2018-06-08 14:30:28 +0800

    Fix search function.

--- src/applications/search/controller/PhabricatorApplicationSearchController.php
+++ src/applications/search/controller/PhabricatorApplicationSearchController.php
@@ -379,7 +379,10 @@ final class PhabricatorApplicationSearchController
                       }
                   }
               }else{
-                  $objects_temp = array_merge($objects_resolved_temp,$objects_released_temp);
+                  if (!($query->getDateResolvedAfter() != 0 ||$query->getDateResolvedBefore() != 0)
+                      &&($query->getDateReleasedAfter() != 0 ||$query->getDateReleasedBefore() != 0)){
+                      $objects_temp = array_merge($objects_resolved_temp,$objects_released_temp);
+                  }
               }
 
               $objects = $objects_temp;
